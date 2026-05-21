2b5e92c9be57b77eb9201b9d403485b2e2b5d88d
dg2026605-collab <dg2026605@danggok.hs.kr>
2026-05-21 09:01:37 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+box(pos = vec(0,1,0),size = vec(5,0.1,1))
+box(pos = vec(2.5,-0.45,0),size = vec(0.1,3,1))
+box(pos = vec(-2.5,-0.45,0),size = vec(0.1,3,1))
+box(pos = vec(0,-1,0),size = vec(5,0.1,1))
+box(pos = vec(0,-0.05,0),size = vec(5,0.1,1))
+box(pos = vec(0,-1.89,0),size = vec(5,0.1,1))
+text(text='Wonder', align='center',pos = vec(0,2,0),height = 0.5,axis = vec(1,0,0))
+box(pos = vec(-2,0.5,0),size = vec(0.7,0.9,0.7))
